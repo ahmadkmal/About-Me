6959330bf35c9266344c75a67190f5a9fdf2e6ac
ahmad kmal <60567529+ahmadkmal@users.noreply.github.com>
2020-02-22 23:27:51 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,78 +1,77 @@
 'use strict';
 var correctAnswer = 0;
-// do {
+var ageans = prompt(' am i a 25 years old ').toLowerCase();
+
 var whatToPrint = '';
-var userName ;
-function ageQuestion() {
-  var ageans = prompt(' am i a 25 years old ').toLowerCase();
-  // } while (ageans !== ('y')&&ageans !== ('yes')&&ageans !== ('n')&&ageans !== ('no'));
+
+if (ageans === 'y' || ageans === 'yes') {
+  alert('your answer was right :) I am 25 years old');
+  correctAnswer++;
+  whatToPrint = '<p> am i a 25 years old ? </P><br> <p> your answer : </p> <p style="color:green;">' + ageans + '</p>';
+}
+else {
+  alert('your answer was worng :( I am 25 years old');
+  whatToPrint = '<p> am i a 25 years old ? </P><br> <p> your answer : </p> <p style="color:red;">' + ageans + '</p>';
+}
+document.write(whatToPrint);
+var aians = prompt(' can i use AI ').toLowerCase();
 
 
-  console.log(' the user answer was ' + ageans);
+if (aians === 'n' || aians === 'no') {
+  alert('your answer was right :) I can\'t ');
+  correctAnswer++;
+  whatToPrint = '<p> can i use AI  ? </P><br> <p> your answer : </p> <p style="color:green;">' + aians + '</p>';
+} else {
+  alert('your answer was worng :( I can\'t');
+  whatToPrint = '<p> can i use AI  ? </P><br> <p> your answer : </p> <p style="color:red;">' + aians + '</p>';
+}
+document.write(whatToPrint);
+var ardans = prompt(' can i programe arduino ').toLowerCase();
 
-  if (ageans === 'y' || ageans === 'yes') {
-    alert('your answer was right :) I am 25 years old');
-    correctAnswer++;
-    whatToPrint = '<p> am i a 25 years old ? </P><br> <p> your answer : </p> <p style="color:green;">' + ageans + '</p>';
-  }
-  else {
-    alert('your answer was worng :( I am 25 years old');
-    whatToPrint = '<p> am i a 25 years old ? </P><br> <p> your answer : </p> <p style="color:red;">' + ageans + '</p>';
-  }
-  // document.write('<p>' + 'index is ' + i + ' ' + weekDays[i] + '</p>');
-  document.write(whatToPrint);
+
+if (ardans === 'y' || ardans === 'yes') {
+  alert('your answer was right :) I can');
+  correctAnswer++;
+  whatToPrint = '<p> can i programe arduino  ? </P><br> <p> your answer : </p> <p style="color:green;">' + ardans + '</p>';
+} else {
+  alert('your answer was worng :( I can');
+  whatToPrint = '<p> can i programe arduino  ? </P><br> <p> your answer : </p> <p style="color:red;">' + ardans + '</p>';
 }
 ageQuestion();
 
-function aiQuestion() {
+document.write(whatToPrint);
+var burans = prompt(' do I looooooove burger ').toLowerCase();
 
-  // do {
-  var aians = prompt(' can i use AI ').toLowerCase();
 
-  // } while (aians !== ('y') && aians !== ('yes') && aians !== ('n') && aians !== ('no'));
-
-  console.log(' the user answer was ' + aians);
-  if (aians === 'n' || aians === 'no') {
-    alert('your answer was right :) I can\'t ');
-    correctAnswer++;
-    whatToPrint = '<p> can i use AI  ? </P><br> <p> your answer : </p> <p style="color:green;">' + aians + '</p>';
-  } else {
-    alert('your answer was worng :( I can\'t');
-    whatToPrint = '<p> can i use AI  ? </P><br> <p> your answer : </p> <p style="color:red;">' + aians + '</p>';
-  }
-  document.write(whatToPrint);
+if (burans === 'y' || burans === 'yes') {
+  alert('your answer was right :)I loooove it');
+  correctAnswer++;
+  whatToPrint = '<p>  do I looooooove burger ? </P><br> <p> your answer : </p> <p style="color:green;">' + burans + '</p>';
+} else {
+  alert('your answer was worng :( I loooooove it');
+  whatToPrint = '<p>  do I looooooove burger ? </P><br> <p> your answer : </p> <p style="color:green;">' + burans + '</p>';
 }
 aiQuestion();
 
-function arduinoQuestion() {
+document.write(whatToPrint);
+var sweet = prompt(' do i like sweet ').toLowerCase();
 
-  // do {
-  var ardans = prompt(' can i programe arduino ').toLowerCase();
 
-  // } while (ardans !== 'y' && ardans !== 'yes' && ardans !== 'n' && ardans !== 'no');
-
-  console.log(' the user answer was ' + ardans);
-  if (ardans === 'y' || ardans === 'yes') {
-    alert('your answer was right :) I can');
-    correctAnswer++;
-    whatToPrint = '<p> can i programe arduino  ? </P><br> <p> your answer : </p> <p style="color:green;">' + ardans + '</p>';
-  } else {
-    alert('your answer was worng :( I can');
-    whatToPrint = '<p> can i programe arduino  ? </P><br> <p> your answer : </p> <p style="color:red;">' + ardans + '</p>';
-  }
-
-  document.write(whatToPrint);
+if (sweet === 'n' || sweet === 'no') {
+  alert('your answer was right :) I dont like it');
+  correctAnswer++;
+  whatToPrint = '<p> do i like sweet ? </P><br> <p> your answer : </p> <p style="color:green;">' + sweet + '</p>';
+} else {
+  alert('your answer was worng :( I dont like it');
+  whatToPrint = '<p> do i like sweet ? </P><br> <p> your answer : </p> <p style="color:red;">' + sweet + '</p>';
 }
 arduinoQuestion();
 
 function burgerQuestion() {
 
-  // do {
   var burans = prompt(' do I looooooove burger ').toLowerCase();
 
-  // } while (burans !== 'y' & burans !== 'yes' & burans !== 'n' & burans !== 'no');
 
-  console.log(' the user answer was ' + burans);
   if (burans === 'y' || burans === 'yes') {
     alert('your answer was right :)I loooove it');
     correctAnswer++;
@@ -81,6 +80,10 @@ function burgerQuestion() {
     alert('your answer was worng :( I loooooove it');
     whatToPrint = '<p>  do I looooooove burger ? </P><br> <p> your answer : </p> <p style="color:green;">' + burans + '</p>';
   }
+  whatToPrint = whatToPrint + ' <br>  <p style="color:red;">' + countryVisted + '</p>';
+
+  if (Number(countryVisted) < 9) {
+    alert('too low');
 
   document.write(whatToPrint);
 }
@@ -88,12 +91,9 @@ burgerQuestion();
 
 function sweetsQuestion() {
 
-  // do {
   var sweet = prompt(' do i like sweet ').toLowerCase();
 
-  // } while (sweet !== 'y' & sweet !== 'yes' & sweet !== 'n' & sweet !== 'no');
 
-  console.log(' the user answer was ' + sweet);
   if (sweet === 'n' || sweet === 'no') {
     alert('your answer was right :) I dont like it');
     correctAnswer++;
@@ -112,25 +112,18 @@ function nameRequest() {
   whatToPrint = '<p> how much country i have been visited ? </P><br> <p> your answer : </p> >';
   return (userName);
 }
-// nameRequest();
+document.write(whatToPrint);
+alert('the right answer is 9');
 
-function travelQuestion() {
-
-  for (var i = 0; i < 4; i++) {
-    var countryVisted = prompt(' how much country i have been visited  ');
-
-    if (Number(countryVisted) === 9) {
-      correctAnswer++;
-      whatToPrint = whatToPrint + '<br>  <p style="color:green;">' + countryVisted + '</p>';
+var favFoods = ['mansaf', 'shawirma', 'burger', 'steak'];
+var arrCorrect = false;
+whatToPrint = '<p> What\'s my fav food? ? </P><br> <p> your answer : </p> >';
+for (var c = 0; c < 6; c++) {
+  var myFav = prompt('What\'s my fav food?').toLowerCase();
+  for (var j = 0; j < favFoods.length; j = j + 1) {
+    if (myFav === favFoods[j]) {
+      arrCorrect = true;
       break;
-    }
-    whatToPrint = whatToPrint + ' <br>  <p style="color:red;">' + countryVisted + '</p>';
-    console.log(Number(countryVisted));
-    if (Number(countryVisted) < 9) {
-      alert('too low');
-
-    } else {
-      alert('too high');
     }
 
   }
@@ -148,13 +141,9 @@ function foodQuestion() {
     var myFav = prompt('What\'s my fav food?').toLowerCase();
     for (var j = 0; j < favFoods.length; j = j + 1) {
       if (myFav === favFoods[j]) {
-      //   console.log('yes you are True !');
         arrCorrect = true;
-        console.log('it' + j + 'index');
         break;
-      } else {
-        console.log('Not ' + j + 'idex');
-      }
+      } 
     }
     if (arrCorrect === true) {
       whatToPrint = whatToPrint + '<br>  <p style="color:green;">' + myFav + '</p>';
@@ -169,5 +158,4 @@ function foodQuestion() {
 }
 foodQuestion();
 alert('thank you ' + nameRequest() + ' to play this game . your scoure is (' + correctAnswer + '/7)');
-// document.write('<p> thank tou ' + userName + ' to play this game . your scoure is (' + correctAnswer + '/7) <p><br><br>');
 
